91fc3cc9412dea078c48fa089b1afa373a0af7d6
xujie <jyoketsu@gmail.com>
2022-08-16 14:30:02 +0800

    fix: 匹配并修改被p包裹的img;2.0版本，因为<p><img>之前可能会有其他标签

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-import { removeImgP } from "./utils/util";
+import { removeImgP, removeImgP2 } from "./utils/util";
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.type == "clip") {
@@ -24,7 +24,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
       options.content = `<h1>${options.title}</h1>` + getSelectedContents();
     } else if (message.clipType === "clip-content") {
       // 采集全文
-      const content = removeImgP(article.content);
+      const content = removeImgP2(article.content);
       options.content = `<h1>${options.title}</h1>` + content;
     } else if (message.clipType === "clip-url") {
       // 保存网址
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -19,3 +19,34 @@ export const removeImgP = (html: string) => {
   }
   return res;
 };
+
+/**
+ * 上述方法的2.0版本，因为<p><img>之前可能会有其他标签
+ * @param html 原始html字符串
+ * @returns res 处理后的html字符串
+ */
+export const removeImgP2 = (html: string) => {
+  let res = html;
+  const imgReg = /<img.*?(?:>|\/>)/gi;
+  if (!imgReg.test(res)) {
+    return res;
+  }
+  // 去除标签之间的空格（包含全角）
+  const tagSpaceReg = /(>\s+<)|(>[　]+<)/gi;
+  res = res.replace(tagSpaceReg, "><");
+
+  const array = res.split("</p>");
+  res = "";
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    let str = `${element}</p>`;
+    const matchedArr = str.match(imgReg);
+    str = str.replace(imgReg, "");
+    for (let index = 0; matchedArr && index < matchedArr.length; index++) {
+      const img = matchedArr[index];
+      str += img;
+    }
+    res += str;
+  }
+  return res;
+};
